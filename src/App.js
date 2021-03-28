3d6fbd4e9b4daff6c22877deb585610024df38f3
HongSenDu <hongsendu@gmail.com>
2021-03-28 15:52:32 -0400

    added files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import LineChart from './LineChart';
+import LineChartRechart from './LineChartRechart';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [number, setNumber] = useState(0)
+
+  useEffect(() => {
+    regenerateData();
+  }, []);
+
+  function regenerateData() {
+    setNumber(Math.random() * 10)
+    console.log(number)
+    const chartData = [];
+    for (let i = 0; i < 20; i++) {
+      const value = Math.floor(Math.random() * i + 3);
+      chartData.push({
+        label: i,
+        value,
+        tooltipContent: `<b>x: </b>${i}<br><b>y: </b>${value}`
+      });
+    }
+    setData(chartData)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{number}</div>
+      <button onClick={regenerateData}>Change Data</button>
+      <LineChart data={data} width={400} height={300} />
+      <LineChartRechart data={data} width={400} height={300} />
     </div>
   );
 }
